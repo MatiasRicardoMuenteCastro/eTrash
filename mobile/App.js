19cbbdb7d8fb0519a440c10e053fc6b49b591401
Kaique Ryan <kaique.github@gmail.com>
2020-09-01 21:33:56 -0300

    Added Navigation Container for routes component

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,19 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import { StatusBar } from 'react-native';
 
-import Routes from './src/routes/app-routes.js';
+import { NavigationContainer } from '@react-navigation/native';
+
+
+import Routes from './src/routes/routes';
 
 const App = () => {
     return (
         <>
-        <StatusBar backgroundColor="#ffffff" barStyle='dark-content' translucent />
-        <Routes />
+        <StatusBar backgroundColor="#ffffff" barStyle='dark-content' translucent /> 
+        <NavigationContainer>
+        	<Routes />
+        </NavigationContainer>
         </>
      );
     
