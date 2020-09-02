1218c7d7e68d12c494563a0d6f75729e6aa502c6
Kaique Ryan <kaique.github@gmail.com>
2020-09-02 20:11:45 -0300

    I added AsyncStorage clear for clear local device storage

--- mobile/App.js
+++ mobile/App.js
@@ -1,13 +1,20 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
 
+import AsyncStorage from '@react-native-community/async-storage';
 
 import Routes from './src/routes/routes';
 
 const App = () => {
+
+	useEffect( async () => {
+		await AsyncStorage.clear();
+	}, []);
+
+
     return (
         <>
         <StatusBar backgroundColor="#ffffff" barStyle='dark-content' translucent /> 
