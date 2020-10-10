daad4c500646f87c42822f0b34f2bb57bfe5f6b7
MatiasRicardoMuenteCastro <matiascastro@outlook.com.br>
2020-10-09 23:40:28 -0300

    auth

--- backend/src/middlewares/auth.js
+++ backend/src/middlewares/auth.js
@@ -7,21 +7,21 @@ module.exports = (request,response,next) => {
     const authHeader = request.headers.authentication;
 
     if(!authHeader)
-        return response.status(401).json({error:'Token de autorização não informado!'});
+        return response.status(401).send({erro:'Token de autorização não informado!'});
 
     const parts = authHeader.split(' ');
 
     if(!parts.length === 2)
-        return response.status(401).json({error: 'Error de token.'});
+        return response.status(401).send({error: 'Error de token.'});
 
 
     const [scheme, token] = parts;
 
     if(!/^Bearer$/i.test(scheme))
-        return response.status(401).json({error: 'Token mau formatado!'});
+        return response.status(401).send({error: 'Token mau formatado!'});
     
-    jwt.verify(token, authConfig.secret, (err, decoded ) => {
-        if(err) return response.status(401).josn({error: 'Token Inválido'});
+    jwt.verify(token, authConfig.secret, (err,decoded)=>{
+        if(err) return response.status(401).send({error: 'Token Inválido'});
 
         request.userId = decoded.id;
         return next();
